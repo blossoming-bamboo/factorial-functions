b59f1c165ee9789e2a01593939abd6db94d6ce7b
blossoming-bamboo <71766369+blossoming-bamboo@users.noreply.github.com>
2020-09-24 14:20:20 +0300

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,39 +1,52 @@
 def f1(x, y=1, z=1):
-    if x < 0:
-        print("Число должно быть неотрицательным!")
+    if x < 0 or type(x) != int:
+        return "Эррор!!!"
     else:
         while z <= x:
             y *= z
             z += 1
-        print("Факториал числа", x, "равен", y)
+        return "Факториал числа " + str(x) + " равен " + str(y)
 
 
-f1(int(input("Введите число: ")))
+print(f1(int(input("Введите число: "))))
 
 
 def f2(x, y=1):
-    if x < 0:
-        print("Число должно быть неотрицательным!")
-    elif x == 0:
-        print("Факториал 0 равен 1")
-    else:
-        while x > 0:
-            y *= x
-            x -= 1
-
-        print("Факториал равен", y)
+    if x < 0 or type(x) != int:
+        return "Эррор!!!"
+    if x in (0, 1):
+        return "Факториал равен 1"
+    while x > 0:
+        y *= x
+        x -= 1
+    return "Факториал равен " + str(y)
 
 
-f2(int(input("Введите число: ")))
+a = int(input())
+
+print(f2(a))
 
 
 def f3(x, y=1):
-    if x < 0:
-        print("Число должно быть неотрицательным!!")
+    if x < 0 or type(x) != int:
+        return "Эррор!!!"
+    elif x == 0:
+        return "Факториал 0 равен 1"
     else:
         for i in range(1, x+1):
             y *= i
-        print("Факториал:", y)
+        return "Факториал: " + str(y)
 
 
-f3(int(input("Введите число: ")))
+print(f3(int(input("Введите число: "))))
+
+
+def factorial(x):
+    if x in (0, 1):
+        return 1
+    return x * factorial(x - 1)
+
+
+a = int(input("Введите чило: "))
+print(factorial(a))
+
